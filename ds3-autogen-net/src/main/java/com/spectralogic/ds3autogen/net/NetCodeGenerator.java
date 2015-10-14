5585929f65fff6aa80a8d671aa7679c81e098e85
Ryan Moore <ryan.moore@greyrocksoft.com>
2015-10-14 10:27:26 -0600

    Adding stub file for .net code generator

---
+++ ds3-autogen-net/src/main/java/com/spectralogic/ds3autogen/net/NetCodeGenerator.java
@@ -0,0 +1,12 @@
+package com.spectralogic.ds3autogen.net;
+
+import com.spectralogic.ds3autogen.api.CodeGenerator;
+import com.spectralogic.ds3autogen.api.FileUtils;
+import com.spectralogic.ds3autogen.api.models.Ds3ApiSpec;
+
+public class NetCodeGenerator implements CodeGenerator {
+    @Override
+    public void generate(final Ds3ApiSpec spec, final FileUtils fileUtils, final String destDir) {
+
+    }
+}
