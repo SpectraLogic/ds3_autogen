f8e1a16923d99bccb6b9015bbb15a33f5ff060e8
DenverM80 <denverm@spectralogic.com>
2015-10-15 16:07:22 -0600

    checkpoint - ready for Descriptors to interpret the model

---
+++ ds3-autogen-c/src/main/java/com/spectralogic/ds3autogen/c/Descriptors/RequestDescriptor.java
@@ -0,0 +1,8 @@
+package com.spectralogic.ds3autogen.c.Descriptors;
+
+/**
+ * Created by denverm on 10/15/15.
+ */
+public class RequestDescriptor {
+    
+}
---
+++ ds3-autogen-c/src/test/resources/output/InitDeleteBucket.c
@@ -0,0 +1,19 @@
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2015 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+
+ds3_request* ds3_init_delete_bucket(const char* bucket_name) {
+    return (ds3_request*) _common_request_init(HTTP_DELETE, _build_path("/", bucket_name, NULL));
+}
