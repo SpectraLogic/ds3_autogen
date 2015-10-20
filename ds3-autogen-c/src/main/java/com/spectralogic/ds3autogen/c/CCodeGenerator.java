4670b1e2974e521b9693d0337e6bbaf2a0316006
DenverM80 <denverm@spectralogic.com>
2015-10-20 10:16:20 -0600

    eliminate potential NullPointerExceptions until all Ds3Request Classifications are dealt with

--- ds3-autogen-c/src/main/java/com/spectralogic/ds3autogen/c/CCodeGenerator.java
+++ ds3-autogen-c/src/main/java/com/spectralogic/ds3autogen/c/CCodeGenerator.java
@@ -16,6 +16,7 @@ import java.nio.file.Path;
 
 import java.nio.file.Paths;
 
+import freemarker.core.Environment;
 import freemarker.template.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +28,7 @@ public class CCodeGenerator implements CodeGenerator {
 
     private Ds3ApiSpec spec;
     private FileUtils fileUtils;
-    private Path destDir;
+    private Path outputDirectory;
 
     public CCodeGenerator() {
         config.setDefaultEncoding("UTF-8");
@@ -38,7 +39,7 @@ public class CCodeGenerator implements CodeGenerator {
     public void generate(final Ds3ApiSpec spec, final FileUtils fileUtils, final Path destDir) throws IOException {
         this.spec = spec;
         this.fileUtils = fileUtils;
-        this.destDir = destDir;
+        this.outputDirectory = destDir;
 
         try {
             generateCommands();
@@ -49,7 +50,7 @@ public class CCodeGenerator implements CodeGenerator {
 
     private void generateCommands() throws IOException, TemplateException {
         for (final Ds3Request request : spec.getRequests()) {
-            generateInitRequest(request);
+            generateRequest(request);
         }
 
         /*
@@ -59,26 +60,39 @@ public class CCodeGenerator implements CodeGenerator {
         */
     }
 
-    public void generateInitRequest(final Ds3Request ds3Request) throws IOException, TemplateException {
-        Template template = null;
+    public void generateRequest(final Ds3Request ds3Request) throws IOException, TemplateException {
+        Template requestTemplate = null;
         Request request = null;
 
         System.out.println("Generating Request[" + ds3Request.getName() + "]");
         if (ds3Request.getClassification() == Classification.amazons3) {
             request = RequestConverter.toRequest(ds3Request);
             System.out.println("Loading template AmazonS3InitRequestHandler.tmplt");
-            template = config.getTemplate("AmazonS3InitRequestHandler.tmplt");
+
+            requestTemplate = config.getTemplate("AmazonS3InitRequestHandler.tmplt");
             System.out.println("Template loaded!");
-        } else if (ds3Request.getClassification() == Classification.spectrads3) {
-            System.out.println("amazonS3 request");
-        } else if (ds3Request.getClassification() == Classification.spectrainternal) /* && codeGenType != production */ {
-            System.out.println("spectra internal request");
+        //} else if (ds3Request.getClassification() == Classification.spectrads3) {
+        //    System.out.println("amazonS3 request");
+        //} else if (ds3Request.getClassification() == Classification.spectrainternal) /* && codeGenType != production */ {
+        //    System.out.println("spectra internal request");
+        } else {
+            throw new TemplateException("Unknown dDs3Request Classification: " + ds3Request.getClassification().toString(), Environment.getCurrentEnvironment());
         }
 
-        final Path outputPath = Paths.get(destDir + "/ds3_c_sdk/src/DeleteBucketRequestHandler.c");
+        final Path outputPath = getOutputPath(request);
+
         System.out.println("outputPath[" + outputPath.toString() + "]");
         final OutputStream outStream = fileUtils.getOutputFile(outputPath);
         final Writer writer = new OutputStreamWriter(outStream);
-        template.process(request, writer);
+        try {
+            requestTemplate.process(request, writer);
+        } catch ( NullPointerException e) {
+            System.out.println("Encountered NullPointerException while processing template " + requestTemplate.getName());
+            e.printStackTrace();
+        }
+    }
+
+    public Path getOutputPath(final Request request) {
+        return Paths.get(outputDirectory + "/src/" + request.getNameRoot() + "RequestHandler.c");
     }
 }
