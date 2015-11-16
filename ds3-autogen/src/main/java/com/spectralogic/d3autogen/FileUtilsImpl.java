02a79c264e63df1c08dc00b54714955708ca1e9d
Rachel Tucker <rachelt@spectralogic.com>
2015-11-16 11:37:14 -0700

    working on stuff

--- ds3-autogen/src/main/java/com/spectralogic/d3autogen/FileUtilsImpl.java
+++ ds3-autogen/src/main/java/com/spectralogic/d3autogen/FileUtilsImpl.java
@@ -17,12 +17,24 @@ package com.spectralogic.d3autogen;
 
 import com.spectralogic.ds3autogen.api.FileUtils;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileUtilsImpl implements FileUtils {
     @Override
     public OutputStream getOutputFile(final Path path) {
+        //make sure directory is created
+        createDirectories(path.);
         return null;
+    }
+
+    private void createDirectories(final Path path) {
+        try {
+            Files.createDirectories(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
