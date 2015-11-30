c72fd5178677b05f94af0cf8050ca96e208682b4
Rachel Tucker <rachelt@spectralogic.com>
2015-11-30 15:34:33 -0700

    Added unit tests for ConverterUtil class

---
+++ ds3-autogen-java/src/test/java/com/spectralogic/ds3autogen/java/Util_Test.java
@@ -0,0 +1,75 @@
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2015 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+package com.spectralogic.ds3autogen.java;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+
+import static com.spectralogic.ds3autogen.java.utils.ConverterUtil.hasContent;
+import static com.spectralogic.ds3autogen.java.utils.ConverterUtil.isEmpty;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Util_Test {
+
+    @Test
+    public void converterUtilList() {
+        final ImmutableList<String> nullList = null;
+        assertTrue(isEmpty(nullList));
+        assertFalse(hasContent(nullList));
+
+        final ImmutableList<String> emptyList = ImmutableList.of();
+        assertTrue(isEmpty(emptyList));
+        assertFalse(hasContent(emptyList));
+
+        final ImmutableList<String> fullList = ImmutableList.of("One", "Two", "Three");
+        assertTrue(hasContent(fullList));
+        assertFalse(isEmpty(fullList));
+    }
+
+    @Test
+    public void convertUtilMap() {
+        final ImmutableMap<String, String> nullMap = null;
+        assertTrue(isEmpty(nullMap));
+        assertFalse(hasContent(nullMap));
+
+        final ImmutableMap<String, String> emptyMap = ImmutableMap.of();
+        assertTrue(isEmpty(emptyMap));
+        assertFalse(hasContent(emptyMap));
+
+        final ImmutableMap<String, String> fullMap = ImmutableMap.of(
+                "key1", "value1",
+                "key2", "value2");
+        assertTrue(hasContent(fullMap));
+        assertFalse(isEmpty(fullMap));
+    }
+
+    @Test
+    public void convertUtilString() {
+        final String nullString = null;
+        assertTrue(isEmpty(nullString));
+        assertFalse(hasContent(nullString));
+
+        final String emptyString = "";
+        assertTrue(isEmpty(emptyString));
+        assertFalse(hasContent(emptyString));
+
+        final String fullString = "Hello World";
+        assertTrue(hasContent(fullString));
+        assertFalse(isEmpty(fullString));
+    }
+}
