6afd146d2a47e119bfa9b7e6aa70b655d5848a01
Rachel Tucker <rachelt@spectralogic.com>
2015-12-03 12:37:01 -0700

    fixed naming of with-constructors in java create object template

--- ds3-autogen-java/src/test/java/com/spectralogic/ds3autogen/java/JavaCodeGenerator_Test.java
+++ ds3-autogen-java/src/test/java/com/spectralogic/ds3autogen/java/JavaCodeGenerator_Test.java
@@ -601,6 +601,8 @@ public class JavaCodeGenerator_Test {
         assertTrue(isReqParamOfType("ObjectName", "String", requestName, requestGeneratedCode, false));
         assertTrue(isReqParamOfType("Channel", "SeekableByteChannel", requestName, requestGeneratedCode, false));
         assertTrue(isReqParamOfType("Size", "long", requestName, requestGeneratedCode, false));
+        assertTrue(hasMethod("withChecksum", requestName, Scope.PUBLIC, requestGeneratedCode));
+        assertTrue(hasMethod("withMetaData", requestName, Scope.PUBLIC, requestGeneratedCode));
 
         assertTrue(hasImport("com.spectralogic.ds3client.HttpVerb", requestGeneratedCode));
         assertTrue(hasImport("com.spectralogic.ds3client.models.Checksum", requestGeneratedCode));
