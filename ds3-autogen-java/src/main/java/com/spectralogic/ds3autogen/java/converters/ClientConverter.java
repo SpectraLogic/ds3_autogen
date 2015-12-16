7de8340d239df38c595cbf2f26b2048210dfd1c0
Rachel Tucker <rachelt@spectralogic.com>
2015-12-16 10:33:32 -0700

    Added documentation to ClientConverter and improved function naming schemes

--- ds3-autogen-java/src/main/java/com/spectralogic/ds3autogen/java/converters/ClientConverter.java
+++ ds3-autogen-java/src/main/java/com/spectralogic/ds3autogen/java/converters/ClientConverter.java
@@ -22,6 +22,10 @@ import com.spectralogic.ds3autogen.java.models.Command;
 
 import static com.spectralogic.ds3autogen.utils.ConverterUtil.isEmpty;
 
+/**
+ * Converts a list of Ds3Requests into a Client model used for generating
+ * the Ds3Client.java and Ds3ClientImpl.java
+ */
 public class ClientConverter {
 
     private final String packageName;
@@ -34,26 +38,41 @@ public class ClientConverter {
         this.packageName = packageName;
     }
 
+    /**
+     * Converts data stored within this ClientConverter into a Client model
+     * @return A Client model
+     */
     private Client convert() {
         return new Client(
                 packageName,
-                getCommands(ds3Requests));
+                toCommands(ds3Requests));
     }
 
-    protected static ImmutableList<Command> getCommands(final ImmutableList<Ds3Request> ds3Requests) {
+    /**
+     * Converts a list of Ds3Requests into a list of Commands
+     * @param ds3Requests List of Ds3Requests
+     * @return A list of Commands
+     */
+    protected static ImmutableList<Command> toCommands(final ImmutableList<Ds3Request> ds3Requests) {
         if (isEmpty(ds3Requests)) {
             return ImmutableList.of();
         }
         final ImmutableList.Builder<Command> builder = ImmutableList.builder();
         for (final Ds3Request ds3Request : ds3Requests) {
             builder.add(new Command(
-                    getCommandName(ds3Request.getName()),
+                    toCommandName(ds3Request.getName()),
                     removePath(ds3Request.getName()),
-                    getResponseName(ds3Request.getName())));
+                    toResponseName(ds3Request.getName())));
         }
         return builder.build();
     }
 
+    /**
+     * Converts a list of Ds3Requests and a package name into a Client model
+     * @param ds3Requests List of Ds3Requests
+     * @param packageName The name of java package for the generated Client
+     * @return A Client model containing information of the Ds3Requests and package name
+     */
     public static Client toClient(
             final ImmutableList<Ds3Request> ds3Requests,
             final String packageName) {
@@ -62,14 +81,30 @@ public class ClientConverter {
         return converter.convert();
     }
 
-    private static String getCommandName(final String requestName) {
+    /**
+     * Converts a Ds3Request's name into a Command name
+     * @param requestName The name of a Ds3Request
+     * @return The Command name
+     */
+    private static String toCommandName(final String requestName) {
         return removePath(requestName).replace("Request", "");
     }
 
-    private static String getResponseName(final String requestName) {
+    /**
+     * Converts a Ds3Request's name into a Response name
+     * @param requestName The name of a Ds3Request
+     * @return The Response name
+     */
+    private static String toResponseName(final String requestName) {
         return removePath(requestName).replace("Request", "Response");
     }
 
+    /**
+     * Removes the path from a string. This is used to remove the contract path
+     * path from Ds3Requests.
+     * @param str A string
+     * @return The subset of str that is located after the last period '.' within str
+     */
     private static String removePath(final String str) {
         final String[] classParts = str.split("\\.");
         return classParts[classParts.length - 1];
