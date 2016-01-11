14afa9a17f694648516b88f759c715c215b5fed3
DenverM80 <denverm@spectralogic.com>
2016-01-11 16:55:16 -0700

    Add a few tests for ds3_autogen_c::Type

--- ds3-autogen-c/src/test/java/com/spectralogic/ds3autogen/c/Type_Test.java
+++ ds3-autogen-c/src/test/java/com/spectralogic/ds3autogen/c/Type_Test.java
@@ -1,7 +1,8 @@
-/*
 package com.spectralogic.ds3autogen.c;
 
-import com.spectralogic.ds3autogen.c.models.Element;
+import com.google.common.collect.ImmutableList;
+import com.spectralogic.ds3autogen.api.models.Ds3Element;
+import com.spectralogic.ds3autogen.c.models.Type;
 import org.junit.Test;
 
 import java.text.ParseException;
@@ -10,6 +11,27 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Type_Test {
+    @Test
+    public void testTypeIsPrimitive() throws ParseException {
+        Ds3Element testElement1 = new Ds3Element("intElement", "int", null, null);
+        Ds3Element testElement2 = new Ds3Element("boolElement", "boolean", null, null);
+        final ImmutableList.Builder<Ds3Element> builder = ImmutableList.builder();
+        builder.add(testElement1);
+        builder.add(testElement2);
+        Type testType = new Type("testType", null, builder.build());
+        assertThat(testType.isPrimitiveType(), is(true));
+    }
+    @Test
+    public void testTypeIsNotPrimitive() throws ParseException {
+        Ds3Element testElement1 = new Ds3Element("boolElement", "boolean", null, null);
+        Ds3Element testElement2 = new Ds3Element("complexElement", "com.spectralogic.s3.server.domain.UserApiBean", null, null);
+        final ImmutableList.Builder<Ds3Element> builder = ImmutableList.builder();
+        builder.add(testElement1);
+        builder.add(testElement2);
+        Type testType = new Type("testType", null, builder.build());
+        assertThat(testType.isPrimitiveType(), is(false));
+    }
+    /*
     @Test
     public void testElementTypeArrayToString() throws ParseException {
         Element stringElement = new Element("arrayElement", "array", null, null);
@@ -20,5 +42,5 @@ public class Type_Test {
         Element stringElement = new Element("setElement", "java.util.Set", null, null);
         assertThat(CHelper.elementTypeToString(stringElement), is(""));
     }
+    */
 }
-*/
