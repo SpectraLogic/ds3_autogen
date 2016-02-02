010da466e7fa39f4ed61b1b304107aec986f4f41
Rachel Tucker <rachelt@spectralogic.com>
2016-02-02 13:01:38 -0700

    Updated Name Mapper and associated unit tests

--- ds3-autogen-parser/src/test/java/com/spectralogic/ds3autogen/Ds3SpecConverter_Test.java
+++ ds3-autogen-parser/src/test/java/com/spectralogic/ds3autogen/Ds3SpecConverter_Test.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static com.spectralogic.ds3autogen.Ds3SpecConverter.*;
 import static com.spectralogic.ds3autogen.testutil.Ds3ModelPartialDataFixture.createDs3TypeTestData;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -34,72 +35,166 @@ public class Ds3SpecConverter_Test {
     private static final String TEST_NAME_MAPPER_FILE = "/testTypeNameMap.json";
 
     @Test
-    public void convertName() throws IOException, ParserException {
+    public void getPathFromName_Test() throws IOException, ParserException {
+        assertThat(getPathFromName(null), is(""));
+        assertThat(getPathFromName(""), is(""));
+        assertThat(getPathFromName("test"), is(""));
+        assertThat(getPathFromName("com.spectralogic.test"), is("com.spectralogic."));
+        assertThat(getPathFromName("com.spectralogic.test$test2"), is("com.spectralogic."));
+    }
+
+    @Test
+    public void getNameFromPath_Test() throws IOException, ParserException {
+        assertThat(getNameFromPath(null), is(""));
+        assertThat(getNameFromPath(""), is(""));
+        assertThat(getNameFromPath("test"), is("test"));
+        assertThat(getNameFromPath("com.spectralogic.test"), is("test"));
+        assertThat(getNameFromPath("com.spectralogic.test$test2"), is("test$test2"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getNameFromPath_Exception_Test() throws IOException, ParserException {
+        getNameFromPath("test.");
+    }
+
+    @Test
+    public void convertName_NullName_Test() throws IOException, ParserException {
+        final String result = Ds3SpecConverter.convertName(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertName_EmptyName_Test() throws IOException, ParserException {
+        final String result = Ds3SpecConverter.convertName("", null);
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void convertName_NoPath_Test() throws IOException, ParserException {
+        final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
+
+        final String expectedResult = "TEST_BlobStoreTaskPriority";
+        final String input = "BlobStoreTaskPriority";
+        final String result = Ds3SpecConverter.convertName(input, nameMapper);
+
+        assertThat(result, is(expectedResult));
+    }
+
+    @Test
+    public void convertName_WithPath_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final String expectedResult = "com.spectralogic.s3.common.dao.domain.ds3.TEST_BlobStoreTaskPriority";
         final String input = "com.spectralogic.s3.common.dao.domain.ds3.BlobStoreTaskPriority";
         final String result = Ds3SpecConverter.convertName(input, nameMapper);
 
-        assertThat(expectedResult, is(result));
+        assertThat(result, is(expectedResult));
     }
 
     @Test
-    public void convertNameWithDollarSign() throws IOException, ParserException {
+    public void convertName_WithDollarSign_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
-        final String expectedResult = "com.spectralogic.s3.common.dao.domain.ds3.TEST_BlobStoreTaskPriority$Test";
-        final String input = "com.spectralogic.s3.common.dao.domain.ds3.BlobStoreTaskPriority$Test";
-        final String result = Ds3SpecConverter.convertNameWithDollarSign(input, nameMapper);
+        final String expectedResult = "com.spectralogic.s3.common.dao.domain.ds3.Test$TEST_BlobStoreTaskPriority";
+        final String input = "com.spectralogic.s3.common.dao.domain.ds3.Test$BlobStoreTaskPriority";
+        final String result = Ds3SpecConverter.convertName(input, nameMapper);
 
-        assertThat(expectedResult, is(result));
+        assertThat(result, is(expectedResult));
     }
 
     @Test
-    public void convertAllAnnotationElements() throws IOException, ParserException {
+    public void convertAllAnnotationElements_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3AnnotationElement> result = convertAllAnnotationElements(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertAllAnnotationElements_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3AnnotationElement> result = convertAllAnnotationElements(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertAllAnnotationElements_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3AnnotationElement> input = ImmutableList.of(
                 new Ds3AnnotationElement(
                         "testName",
                         "TEST_VALUE",
-                        "com.spectralogic.s3.common.dao.domain.ds3.BlobStoreTaskPriority$Test"),
+                        "com.spectralogic.s3.common.dao.domain.ds3.Test$BlobStoreTaskPriority"),
                 new Ds3AnnotationElement(
                         "testName",
                         "TEST_VALUE",
                         "com.spectralogic.s3.common.dao.domain.ds3.Bucket"));
-        final ImmutableList<Ds3AnnotationElement> result = Ds3SpecConverter.convertAllAnnotationElements(input, nameMapper);
+        final ImmutableList<Ds3AnnotationElement> result = convertAllAnnotationElements(input, nameMapper);
 
-        assertThat(result.get(0).getValueType(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_BlobStoreTaskPriority$Test"));
+        assertThat(result.get(0).getValueType(), is("com.spectralogic.s3.common.dao.domain.ds3.Test$TEST_BlobStoreTaskPriority"));
         assertThat(result.get(1).getValueType(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_Bucket"));
     }
 
     @Test
-    public void convertAllAnnotations() throws IOException, ParserException {
+    public void convertAllAnnotations_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Annotation> result = convertAllAnnotations(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertAllAnnotations_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Annotation> result = convertAllAnnotations(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertAllAnnotations_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3Annotation> input = ImmutableList.of(
                 new Ds3Annotation("com.spectralogic.s3.common.dao.domain.ds3.Bucket", null),
                 new Ds3Annotation("Integer", null));
-        final ImmutableList<Ds3Annotation> result = Ds3SpecConverter.convertAllAnnotations(input, nameMapper);
+        final ImmutableList<Ds3Annotation> result = convertAllAnnotations(input, nameMapper);
 
         assertThat(result.get(0).getName(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_Bucket"));
         assertThat(result.get(1).getName(), is(input.get(1).getName()));
     }
 
     @Test
-    public void convertAllEnumConstants() throws IOException, ParserException {
+    public void convertAllEnumConstants_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3EnumConstant> result = convertAllEnumConstants(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertAllEnumConstants_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3EnumConstant> result = convertAllEnumConstants(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertAllEnumConstants_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3EnumConstant> input = ImmutableList.of(
                 new Ds3EnumConstant("com.spectralogic.s3.common.dao.domain.ds3.Bucket", null));
-        final ImmutableList<Ds3EnumConstant> result = Ds3SpecConverter.convertAllEnumConstants(input, nameMapper);
+        final ImmutableList<Ds3EnumConstant> result = convertAllEnumConstants(input, nameMapper);
 
         assertThat(result.get(0).getName(), is(input.get(0).getName()));
     }
 
     @Test
-    public void convertAllProperties() throws IOException, ParserException {
+    public void convertAllProperties_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Property> result = Ds3SpecConverter.convertAllProperties(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertAllProperties_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Property> result = Ds3SpecConverter.convertAllProperties(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertAllProperties_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3Property> input = ImmutableList.of(
@@ -118,7 +213,19 @@ public class Ds3SpecConverter_Test {
     }
 
     @Test
-    public void convertAllElements() throws IOException, ParserException {
+    public void convertAllElements_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Element> result = convertAllElements(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertAllElements_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Element> result = convertAllElements(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertAllElements_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3Element> input = ImmutableList.of(
@@ -127,20 +234,32 @@ public class Ds3SpecConverter_Test {
                         "com.spectralogic.s3.common.dao.domain.ds3.Bucket",
                         "com.spectralogic.s3.common.dao.domain.ds3.BlobStoreTaskPriority",
                         null));
-        final ImmutableList<Ds3Element> result = Ds3SpecConverter.convertAllElements(input, nameMapper);
+        final ImmutableList<Ds3Element> result = convertAllElements(input, nameMapper);
 
         assertThat(result.get(0).getType(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_Bucket"));
         assertThat(result.get(0).getComponentType(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_BlobStoreTaskPriority"));
     }
 
     @Test
-    public void convertTypes() throws IOException, ParserException {
+    public void convertTypes_NullMap_Test() throws IOException, ParserException {
+        final ImmutableMap<String, Ds3Type> result = convertTypes(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertTypes_EmptyMap_Test() throws IOException, ParserException {
+        final ImmutableMap<String, Ds3Type> result = convertTypes(ImmutableMap.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertTypes_FullMap_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final Ds3Type type = createDs3TypeTestData("com.spectralogic.s3.common.dao.domain.ds3.Bucket");
         final ImmutableMap.Builder<String, Ds3Type> input = ImmutableMap.builder();
         input.put("com.spectralogic.s3.common.dao.domain.ds3.Bucket", type);
-        final ImmutableMap<String, Ds3Type> result = Ds3SpecConverter.convertTypes(input.build(), nameMapper);
+        final ImmutableMap<String, Ds3Type> result = convertTypes(input.build(), nameMapper);
 
         assertThat(result.get("com.spectralogic.s3.common.dao.domain.ds3.TEST_Bucket"), is(notNullValue()));
         assertThat(result.get("com.spectralogic.s3.common.dao.domain.ds3.Bucket"), is(nullValue()));
@@ -148,7 +267,7 @@ public class Ds3SpecConverter_Test {
     }
 
     @Test
-    public void convertResponseCode() throws IOException, ParserException {
+    public void convertResponseCode_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3ResponseType> responseTypes = ImmutableList.of(
@@ -156,38 +275,74 @@ public class Ds3SpecConverter_Test {
                         "com.spectralogic.s3.common.dao.domain.ds3.Bucket",
                         "com.spectralogic.s3.common.dao.domain.ds3.BlobStoreTaskPriority"));
         final Ds3ResponseCode input = new Ds3ResponseCode(200, responseTypes);
-        final Ds3ResponseCode result = Ds3SpecConverter.convertResponseCode(input, nameMapper);
+        final Ds3ResponseCode result = convertResponseCode(input, nameMapper);
 
         assertThat(result.getDs3ResponseTypes().get(0).getType(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_Bucket"));
         assertThat(result.getDs3ResponseTypes().get(0).getComponentType(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_BlobStoreTaskPriority"));
     }
 
     @Test
-    public void convertAllResponseCodes() throws IOException, ParserException {
+    public void convertAllResponseCodes_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3ResponseCode> result = convertAllResponseCodes(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertAllResponseCodes_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3ResponseCode> result = convertAllResponseCodes(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertAllResponseCodes_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3ResponseCode> input = ImmutableList.of(
                 new Ds3ResponseCode(200, null));
-        final ImmutableList<Ds3ResponseCode> result = Ds3SpecConverter.convertAllResponseCodes(input, nameMapper);
+        final ImmutableList<Ds3ResponseCode> result = convertAllResponseCodes(input, nameMapper);
 
         assertThat(result, is(notNullValue()));
     }
 
     @Test
-    public void convertAllParams() throws IOException, ParserException {
+    public void convertAllParams_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Param> result = convertAllParams(null, null);
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void convertAllParams_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Param> result = convertAllParams(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertAllParams_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3Param> input = ImmutableList.of(
                 new Ds3Param(
                         "testName",
                         "com.spectralogic.s3.common.dao.domain.ds3.Bucket"));
-        final ImmutableList<Ds3Param> result = Ds3SpecConverter.convertAllParams(input, nameMapper);
+        final ImmutableList<Ds3Param> result = convertAllParams(input, nameMapper);
 
         assertThat(result.get(0).getType(), is("com.spectralogic.s3.common.dao.domain.ds3.TEST_Bucket"));
     }
 
     @Test
-    public void convertRequests() throws IOException, ParserException {
+    public void convertRequests_NullList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Request> result = convertRequests(null, null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertRequests_EmptyList_Test() throws IOException, ParserException {
+        final ImmutableList<Ds3Request> result = convertRequests(ImmutableList.of(), null);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void convertRequests_FullList_Test() throws IOException, ParserException {
         final NameMapper nameMapper = new NameMapper(TEST_NAME_MAPPER_FILE);
 
         final ImmutableList<Ds3Request> input = ImmutableList.of(
@@ -205,7 +360,7 @@ public class Ds3SpecConverter_Test {
                         null,
                         null,
                         null));
-        final ImmutableList<Ds3Request> result = Ds3SpecConverter.convertRequests(input, nameMapper);
+        final ImmutableList<Ds3Request> result = convertRequests(input, nameMapper);
 
         assertThat(result, is(notNullValue()));
     }
