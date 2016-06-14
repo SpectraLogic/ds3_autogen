7cce907fc401f39fc3a3924bbea4f53e38298f72
DenverM80 <denverm@spectralogic.com>
2016-06-14 11:14:36 -0600

    Fix parameter validation for requests that only require a bucket_id

--- ds3-autogen-c/src/main/java/com/spectralogic/ds3autogen/c/helpers/RequestHelper.java
+++ ds3-autogen-c/src/main/java/com/spectralogic/ds3autogen/c/helpers/RequestHelper.java
@@ -142,7 +142,7 @@ public final class RequestHelper {
                 builder.append(indent(2)).append("return ds3_create_error(DS3_ERROR_MISSING_ARGS, \"The object name parameter is required.\");\n");
                 builder.append(indent(1)).append("}");
             } else if(request.isResourceRequired()) {
-                builder.append(indent(1)).append("if (g_ascii_strncasecmp(request->path->value, \"//\", 2) == 0) {\n");
+                builder.append(indent(1)).append("if (request->path->size < 2) {\n");
                 builder.append(indent(2)).append("return ds3_create_error(DS3_ERROR_MISSING_ARGS, \"The bucket name parameter is required.\");\n");
                 builder.append(indent(1)).append("}");
             }
@@ -155,7 +155,7 @@ public final class RequestHelper {
                 builder.append(indent(2)).append("return ds3_create_error(DS3_ERROR_MISSING_ARGS, \"The resource id parameter is required.\");\n");
                 builder.append(indent(1)).append("}");
             } else if(request.isResourceIdRequired()) {
-                builder.append(indent(1)).append("if (g_ascii_strncasecmp(request->path->value, \"//\", 2) == 0) {\n");
+                builder.append(indent(1)).append("if (request->path->size < 2) {\n");
                 builder.append(indent(2)).append("return ds3_create_error(DS3_ERROR_MISSING_ARGS, \"The resource type parameter is required.\");\n");
                 builder.append(indent(1)).append("}");
             }
