4cbb69c80e1e37574a8470d651a8f27791819578
Rachel Tucker <rachelt@spectralogic.com>
2017-08-21 16:53:35 -0600

    Cleaned up net request handler special casing

--- ds3-autogen-net/src/main/resources/tmpls/net/request/bulk_put_request.ftl
+++ ds3-autogen-net/src/main/resources/tmpls/net/request/bulk_put_request.ftl
@@ -1,12 +1,10 @@
 <#include "../common/copyright.ftl" />
 
+using Ds3.Calls.Util;
 using Ds3.Models;
-using Ds3.Runtime;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml.Linq;
 
 namespace Ds3.Calls
 {
@@ -24,8 +22,18 @@ namespace Ds3.Calls
 
         <#include "common/optional_args.ftl" />
 
-        <#include "common/objects_constructor_get_stream.ftl" />
+        <#include "common/constructor.ftl" />
 
         <#include "common/http_verb_and_path.ftl" />
+
+        internal override Stream GetContentStream()
+        {
+            return RequestPayloadUtil.MarshalDs3ObjectNameAndSize(this.Objects);
+        }
+
+        internal override long GetContentLength()
+        {
+            return GetContentStream().Length;
+        }
     }
 }
---
+++ ds3-autogen-net/src/test/java/com/spectralogic/ds3autogen/net/generators/requestmodels/IdsRequestPayloadGenerator_Test.java
@@ -0,0 +1,37 @@
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2017 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+package com.spectralogic.ds3autogen.net.generators.requestmodels;
+
+import com.google.common.collect.ImmutableList;
+import com.spectralogic.ds3autogen.api.models.Arguments;
+import org.junit.Test;
+
+import static com.spectralogic.ds3autogen.testutil.Ds3ModelFixtures.clearSuspectBlobAzureTargetsRequest;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class IdsRequestPayloadGenerator_Test {
+
+    private static final IdsRequestPayloadGenerator generator = new IdsRequestPayloadGenerator();
+
+    @Test
+    public void toConstructorArgsList_Test() {
+        final ImmutableList<Arguments> result = generator.toConstructorArgsList(clearSuspectBlobAzureTargetsRequest());
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0).getName(), is("Ids"));
+        assertThat(result.get(0).getType(), is("IEnumerable<string>"));
+    }
+}
