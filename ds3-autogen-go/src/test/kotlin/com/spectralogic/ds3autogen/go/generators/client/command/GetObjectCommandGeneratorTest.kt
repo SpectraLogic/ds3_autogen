3c6ac5bbdd8bd689497746c201330fac287156fc
Rachel Tucker <rachelt@spectralogic.com>
2017-10-31 13:31:52 -0600

    Special cased Get Object client command generation in Go module

---
+++ ds3-autogen-go/src/test/kotlin/com/spectralogic/ds3autogen/go/generators/client/command/GetObjectCommandGeneratorTest.kt
@@ -0,0 +1,40 @@
+/*
+ * ******************************************************************************
+ *   Copyright 2014-2017 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+package com.spectralogic.ds3autogen.go.generators.client.command
+
+import com.spectralogic.ds3autogen.go.models.client.CustomBuildLine
+import com.spectralogic.ds3autogen.go.models.client.RequestBuildLine
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class GetObjectCommandGeneratorTest {
+
+    private val generator = GetObjectCommandGenerator()
+
+    @Test
+    fun toHeadersBuildLineTest() {
+        Assertions.assertThat<RequestBuildLine>(generator.toHeadersBuildLine())
+                .isNotEmpty
+                .containsSame(CustomBuildLine.METADATA_BUILD_LINE)
+    }
+
+    @Test
+    fun toChecksumBuildLineTest() {
+        Assertions.assertThat<RequestBuildLine>(generator.toChecksumBuildLine())
+                .isNotEmpty
+                .containsSame(CustomBuildLine.CHECKSUM_BUILD_LINE)
+    }
+}
